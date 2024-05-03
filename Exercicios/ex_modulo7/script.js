2cd03f052cf2797ba939f962771dc44b5e288df2
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-05-02 23:03:54 -0300

    commit_html_css

---
+++ Exercicios/ex_modulo7/script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('formVerifica');
+
+function validaCampos() {
+    let numA = document.getElementById('numA').value;
+    let numB = document.getElementById('numB').value;
+
+    if(numA > numB) {
+        
+    }
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+})
+
+console.log(form)
