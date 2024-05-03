6f5da9ad250f54c05f2553216aadf1826831edf2
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-05-03 11:27:58 -0300

    commit_ex_modulo_07

--- Exercicios/ex_modulo7/script.js
+++ Exercicios/ex_modulo7/script.js
@@ -1,10 +1,21 @@
 function verificar() {
     let numA = document.getElementById('numA');
     let numB = document.getElementById('numB');
+    let mensagemErro = `Erro! - Valo de (B) precisa ser maior que valor de (A)!`
+    let mensagemSucesso = `Ok! Valores Validos!`
+    let resMsg = document.querySelector('.res-msg');
 
-    if( numA.value >= numB.value ) {
-        alert('Erro!')
+    if( Number(numB.value) <= Number(numA.value) ) {
+
+        resMsg.innerHTML = mensagemErro;
+        resMsg.style.color = 'red';
+        resMsg.style.display = 'block';
+
     } else {
-        alert('Ok!')
+        mensagemErro = '';
+
+        resMsg.innerHTML = mensagemSucesso;
+        resMsg.style.color = 'green';
+        resMsg.style.display = 'block';
     }
 }
